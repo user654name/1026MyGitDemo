b30e34c06645a8bed919aa8fd13f1df94580c301
aojie.dai <aojie.dai@yintech.cn>
2018-10-26 15:14:01 +0800

    第一次提交

---
+++ DemoModule/src/cn/itcast/test.java
@@ -0,0 +1,14 @@
+package cn.itcast;
+
+/**
+ * @author aojie.dai
+ * @Title: cn.itcast.test
+ * @ProjectName 1026MyGitDemo
+ * @Description: 测试
+ * @date 2018/10/26
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello 火球鼠");
+    }
+}
