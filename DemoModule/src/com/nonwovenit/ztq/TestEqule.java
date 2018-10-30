2c54117f727a72cdfcb2695fc1605132e6fdc671
user654name <ztqztq@qq.com>
2018-10-30 09:55:02 +0800

    2018年10月30日 09:54:39  这是一个坑

---
+++ DemoModule/src/com/nonwovenit/ztq/TestEqule.java
@@ -0,0 +1,35 @@
+package com.nonwovenit.ztq;
+
+import org.junit.Test;
+
+/**
+ * Create By ${Tianqi.Zhang}
+ */
+public class TestEqule {
+
+    @Test
+    public void demo1() {
+
+        Integer d1 = 126;
+        Integer d2 = 126;
+        System.out.println(d1==d2);
+
+
+        Integer b1 = 127;
+        Integer b2 = 127;
+        System.out.println(b1==b2);
+
+
+        Integer a1 = 128;
+        Integer a2 = 128;
+        System.out.println(a1==a2);
+
+
+        Integer c1 = 129;
+        Integer c2 = 129;
+        System.out.println(c1==c2);
+
+
+
+    }
+}
